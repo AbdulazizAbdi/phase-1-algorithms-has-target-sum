052183d23ca44710302f8ecea7bd560b27ffbb1c
Abdulaziz Abdi <amigf76@gmail.com>
2024-04-12 19:31:31 +0300

    has target sum lab

--- index.js
+++ index.js
@@ -15,15 +15,18 @@ function hasTargetSum(array, target) {
 hasTargetSum([22, 19, 4, 6, 30], 25);
 
 /* 
-  Write the Big O time complexity of your function here
+  Write the Big O time complexity of your function here:
+  The Big O complexity is O(n²) because its a nested iteration.
 */
 
 /* 
   Add your pseudocode here
+
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here:
+  It loops through the array from the first index, adding it to all the other indexes and the sum is compared to the target. if the target is matched it returns true but if it loops through all indexes and adds all other indexes to the fixed number then it returns false as it exists the loop.
 */
 
 // You can run `node index.js` to view these console logs
